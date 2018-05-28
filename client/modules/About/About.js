790689c17efbb4dc9cd6d3ebf898ae5bc6648521
Anna Perkowska <perkowska.a@gmail.com>
2018-05-28 11:46:46 +0200

    Partial update to 22.6

---
+++ client/modules/About/About.js
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+import styles from './About.css';
+
+class About extends Component {
+  render() {
+    return (
+    	<div>
+    		<h2>Something about</h2>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+About.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
